ffda9c1be1b5654952f4f8acc2a738ad3c93e508
TusharSusar <tusharsusar06@gmail.com>
2026-05-21 19:43:29 +0530

    Data access object is created for  patient and Service layer implemted for patient

---
+++ src/main/java/com/student/spring_hibernate_crud/service/PatientService.java
@@ -0,0 +1,28 @@
+package com.student.spring_hibernate_crud.service;
+
+import com.student.spring_hibernate_crud.model.Patient;
+import com.student.spring_hibernate_crud.repository.PatientDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    @Autowired
+    PatientDAO patientDAO;
+
+    public List<Patient> getPatients () {
+        return patientDAO.getPatients();
+    }
+
+    public void addPetient (Patient patient) {
+        patientDAO.addPetient(patient);
+    }
+
+    public Patient getPatientByID (int pid){
+        return patientDAO.getPatientByID(pid);
+    }
+
+}
