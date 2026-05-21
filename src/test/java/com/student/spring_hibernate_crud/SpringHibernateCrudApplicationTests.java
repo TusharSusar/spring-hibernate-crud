7bc653140920e56ff60ab92193987f31ae21eaff
TusharSusar <tusharsusar06@gmail.com>
2026-05-21 13:34:18 +0530

    Configuation Done. Intial Commit

---
+++ src/test/java/com/student/spring_hibernate_crud/SpringHibernateCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.spring_hibernate_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
